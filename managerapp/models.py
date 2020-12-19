7e640bb2a1bd1323082f76a60ea71375832932f4
Hp <lolitakur72@gmail.com>
2020-12-19 10:42:42 +0500

    12

--- managerapp/models.py
+++ managerapp/models.py
@@ -3,11 +3,11 @@ from django.db import models
 class Managers(models.Model):
     name = models.CharField(max_length=200)
     competence = models.TextField(blank=True)
-    photo = models.ImageField('Фото менеджера', upload_to='static/img', null=True)
+
 
     def __str__(self):
         return self.name
 
     class Meta:
-        verbose_name = 'Мероприятие'
-        verbose_name_plural = 'Мероприятия'
+        verbose_name = 'Менеджер'
+        verbose_name_plural = 'Менеджеры'
